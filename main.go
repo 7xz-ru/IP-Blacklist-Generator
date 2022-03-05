4e50d1d02affee5cb6231cc8447e9775a900b22d
ZenDarkmaster <zendarkmaster@gmail.com>
2022-03-06 03:49:28 +0700

    Implemented basic idea (backend)

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"os"
 	"io"
 	"path/filepath"
+  "strings"
+  //"strconv"
 )
 //------------------------------------------------------
-var Zone string = "test"
+  // globalvars
 //------------------------------------------------------
 func main() {
   httpServer()
@@ -54,21 +56,55 @@ func read(fd_r io.Reader) ([]byte, error) {
 	}
 	return buf.Bytes(), nil
 }
+// read zonefile func
+func readPrintZone(key string) (re []byte) {
+
+    path := "./zones/" + key + ".zone"
+  	ba ,err := readFile(path)
+  	if err != nil {
+  		fmt.Sprintln("Error: %s\n", err)
+  	}
+
+  return ba
+
+}
+// end read zonefile func
 //------------------------------------------------------
 func httpZone(w http.ResponseWriter, r *http.Request) {
-  //fmt.Fprintf(w, "Github: ZenDarkmaster")
+  fmt.Fprintf(w, "# Github: https://github.com/7xz-ru/IP-Blacklist-Generator\n")
+  fmt.Fprintf(w, "# Author: https://github.com/ZenDarkmaster\n")
+  // get url keys z param
+  keys, ok := r.URL.Query()["z"]
 
-  path := "./zones/" + Zone + ".zone"
-	ba ,err := readFile(path)
-	if err != nil {
-		fmt.Fprintln(w, "Error: %s\n", err)
-	}
-	fmt.Fprintf(w, "# Country ZONE - '%s' : \n%s\n", Zone, ba)
+  if !ok || len(keys[0]) < 1 {
+      fmt.Fprintf(w, "Url Param 'z' is missing")
+      return
+  }
+    // Query()["key"] will return an array of items,
+    // we only want the single item.
+  key := keys[0]
+  // end get url keys z param
 
+  //zones list
+  fmt.Fprintf(w, "# Feed zones: " + string(key) + "\n")
+  //end zones list
+
+  // string key to array words
+  words := strings.Fields(key)
+  // end string key to array words
+
+  // start array cycle and print zonefile
+  for i := 0; i < len(words); i++ {
+      mkey := words[i]
+      fmt.Fprintf(w, "# Zone - %s: \n%s", mkey, readPrintZone(mkey))
+  }
+  // end start array cycle and print zonefile
 }
 //------------------------------------------------------
 func httpServer() {
-  http.HandleFunc("/", httpZone)
+  http.HandleFunc("/ipfeed", httpZone)
   http.ListenAndServe(":8080", nil)
 }
 //------------------------------------------------------
+// for example, open the page, to add zones, use the z parameter in the url
+// example: http://localhost:8080/ipfeed?z=gb+it+fr
